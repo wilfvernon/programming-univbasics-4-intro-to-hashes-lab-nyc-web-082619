7a24cd94940d99101b7a102f9dda5165bec767f6
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 17:44:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,10 +31,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] do
-    hash[key] += 1
-  end
-  else do
-    hash[key] = 1
+  if hash[key]
+  hash[key] += 1
+  else hash[key] = 1
   end
 end
